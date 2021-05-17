7ff30257ac8d72b5b1c06c76cfa03f6d8c7c005c
Andres Cecilia Luque <andres.luque@revolut.com>
2021-05-17 16:41:03 +0100

    Added comment

--- ClassesSampleApp/Dummy.swift
+++ ClassesSampleApp/Dummy.swift
@@ -1,1 +1,8 @@
-// This is needed because of https://github.com/opencv/opencv/issues/19645
+// Since version 4.4.0, opencv includes swift and objective-c wrappers, so it can be used
+// directly without having to wrap the calls in c++.
+// See: https://github.com/opencv/opencv/pull/17165
+//
+// Such wrappers require linking the swift libraries. In swift projects this happens
+// automatically, but in Objective-C in order to link the swift files it is necessary to
+// add a swift file to the project.
+// See: https://github.com/opencv/opencv/issues/19645
